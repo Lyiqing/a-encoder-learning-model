b6d961c4a97b940c7383d5ad910a061045f034d2
Lyiqing <91250750+Lyiqing@users.noreply.github.com>
2021-09-23 16:53:04 +0800

    Add files via upload

---
+++ ye_model/ye_model.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, division
+import argparse
+import numpy as np
+from sklearn.metrics.cluster import normalized_mutual_info_score as nmi_score
+from sklearn.metrics import adjusted_rand_score as ari_score
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from torch.nn import Linear
+
+from utils import MnistDataset
+
+
+class AE(nn.Module):
+
+    def __init__(self, n_enc_1, n_enc_2, n_enc_3, n_dec_1, n_dec_2, n_dec_3,
+                 n_input, n_z):
+        super(AE, self).__init__()
+
+        # encoder
+        self.enc_1 = Linear(n_input, n_enc_1)
+        self.enc_2 = Linear(n_enc_1, n_enc_2)
+        self.enc_3 = Linear(n_enc_2, n_enc_3)
+
+        self.z_layer = Linear(n_enc_3, n_z)
+
+        # decoder
+        self.dec_1 = Linear(n_z, n_dec_1)
+        self.dec_2 = Linear(n_dec_1, n_dec_2)
+        self.dec_3 = Linear(n_dec_2, n_dec_3)
+
+        self.x_bar_layer = Linear(n_dec_3, n_input)
+
+    def forward(self, x):
+
+        # encoder
+        enc_h1 = F.relu(self.enc_1(x))
+        enc_h2 = F.relu(self.enc_2(enc_h1))
+        enc_h3 = F.relu(self.enc_3(enc_h2))
+
+        z = self.z_layer(enc_h3)
+        z_soft=F.softmax(z,dim=1)
+
+        # decoder
+        dec_h1 = F.relu(self.dec_1(z))
+        dec_h2 = F.relu(self.dec_2(dec_h1))
+        dec_h3 = F.relu(self.dec_3(dec_h2))
+        x_bar = self.x_bar_layer(dec_h3)
+
+        return x_bar, z, z_soft
+
+
+
+
+
+
+def train_ae():
+    model = AE(
+        n_enc_1=500,
+        n_enc_2=500,
+        n_enc_3=1000,
+        n_dec_1=1000,
+        n_dec_2=500,
+        n_dec_3=500,
+        n_input=args.n_input,
+        n_z=args.n_z)
+    '''
+    pretrain autoencoder
+    '''
+    train_loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
+    print(model)
+    model=model.to(device)
+    optimizer = Adam(model.parameters(), lr=0.001)
+    for epoch in range(30):
+        total_loss = 0.
+        for batch_idx, (x, _, _) in enumerate(train_loader):
+            x = x.to(device)
+
+            optimizer.zero_grad()
+            x_bar, z ,_= model(x)
+            loss = F.mse_loss(x_bar, x)
+            total_loss += loss.item()
+
+            loss.backward()
+            optimizer.step()
+
+        print("epoch {} loss={:.4f}".format(epoch,
+                                            total_loss / (batch_idx + 1)))
+        torch.save(model.state_dict(), args.pretrain_path)
+    print("model saved to {}.".format(args.pretrain_path))
+    #############################################################
+    train_loader = DataLoader(
+        dataset, batch_size=args.batch_size, shuffle=False)
+    optimizer = Adam(model.parameters(), lr=0.001,weight_decay=0.01)
+    
+    data = dataset.x
+    y = dataset.y
+    data = torch.Tensor(data).to(device)
+    x_bar, hidden ,_= model(data)
+
+    y_pred=y
+
+    y_pred_last = y_pred
+
+    model.train()
+    for epoch in range(200):
+        total_loss=0
+        if epoch % args.update_interval == 0:
+
+            _, z ,z_soft= model(data)
+            tmp_q=z_soft
+            print(tmp_q.size)
+            
+            
+            y_pred = tmp_q.cpu()
+            y_pred = y_pred.detach().numpy().argmax(1)
+            
+            delta_label = np.sum(y_pred != y_pred_last).astype(
+                np.float32) / y_pred.shape[0]
+            y_pred_last = y_pred
+
+            nmi = nmi_score(y, y_pred)
+            ari = ari_score(y, y_pred)
+            print('Iter {}'.format(epoch), 
+                  ', nmi {:.4f}'.format(nmi), ', ari {:.4f}'.format(ari))
+            if epoch > 0 and delta_label < args.tol:
+                print('delta_label {:.4f}'.format(delta_label), '< tol',
+                      args.tol)
+                print('Reached tolerance threshold. Stopping training.')
+                break
+        print('start')
+        for batch_idx, (x,_, idx) in enumerate(train_loader):
+
+            x = x.to(device)
+            idx = idx.to(device)
+
+            x_bar, z , z_soft= model(x)
+            tmp_q=z_soft
+            
+            y_pred = torch.argmax(tmp_q,dim=1)
+            print(y_pred)
+            
+            z_out=torch.autograd.Variable(torch.zeros(z_soft.size(0),10))
+
+            for i in range(z_soft.size(0)):
+                z_out[i,y_pred[i]]=1
+                
+            
+            optimizer.zero_grad()
+            loss = F.mse_loss(z_soft.to(device), z_out.to(device))
+            total_loss += loss.item()
+            
+            loss.backward()
+            optimizer.step()
+            
+            print("loss={:.4f}".format(epoch,total_loss /(batch_idx + 1)))
+        print("epoch {} loss={:.4f}".format(epoch,total_loss /(batch_idx + 1)))
+    
+    
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description='train',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--n_clusters', default=7, type=int)
+    parser.add_argument('--batch_size', default=256, type=int)
+    parser.add_argument('--n_z', default=10, type=int)
+    parser.add_argument('--dataset', type=str, default='mnist')
+    parser.add_argument('--pretrain_path', type=str, default='data/ae_mnist')
+    parser.add_argument(
+        '--gamma',
+        default=0.1,
+        type=float,
+        help='coefficient of clustering loss')
+    parser.add_argument('--update_interval', default=1, type=int)
+    parser.add_argument('--tol', default=0.001, type=float)
+    args = parser.parse_args()
+    args.cuda = torch.cuda.is_available()
+    print("use cuda: {}".format(args.cuda))
+    device = torch.device("cuda" if args.cuda else "cpu")
+
+    if args.dataset == 'mnist':
+        args.pretrain_path = 'data/ae_mnist.pkl'
+        args.n_clusters = 10
+        args.n_input = 784
+        dataset = MnistDataset()
+    print(args)
+    train_ae()
